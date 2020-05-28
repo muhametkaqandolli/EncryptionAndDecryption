eb21cb15d2899fec22a1d22cd72ee2cee78c0fbd
muhamet kaqandolli <muhamet_kaqandolli@hotmail.com>
2020-05-28 15:28:58 +0200

    Add project files.

---
+++ EnforcingPrivacyWithCryptography/Encryption.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnforcingPrivacyWithCryptography
+{
+    class Encryption
+    {
+        private int _digit1;
+        private int _digit2;
+        private int _digit3;
+        private int _digit4;
+
+        public int Digit1
+        {
+            get
+            {
+                return _digit1;
+            }
+            set
+            {
+                _digit1 = SplitDigits(value, 1);
+            }
+        }
+        public int Digit2
+        {
+            get
+            {
+                return _digit2;
+            }
+            set
+            {
+                _digit2 = SplitDigits(value, 2);
+            }
+        }
+        public int Digit3
+        {
+            get
+            {
+                return _digit3;
+            }
+            set
+            {
+                _digit3 = SplitDigits(value, 3);
+            }
+        }
+        public int Digit4
+        {
+            get
+            {
+                return _digit4;
+            }
+            set
+            {
+                _digit4 = SplitDigits(value, 4);
+            }
+        }
+
+        public Encryption(string number)
+        {
+            
+            if (checkFourDigits(number))
+            {
+                Digit1 = Convert.ToInt32(number);
+                Digit2 = Convert.ToInt32(number);
+                Digit3 = Convert.ToInt32(number);
+                Digit4 = Convert.ToInt32(number);
+               
+            }
+            else
+                throw new Exception("The number you typed was not 4-digit number");
+        }
+
+        private bool checkFourDigits(string number)
+        {
+            if (number.Length != 4)
+                return false;
+            else
+                return true;
+        }
+
+        private int SplitDigits(int value, int digit)
+        {
+
+            string stringValue = value.ToString();
+
+
+            int newDigit = Convert.ToInt32(stringValue.Substring(digit - 1, 1));
+
+            return (newDigit + 7) % 10;
+        }
+
+
+        public string EncryptedNumber()
+        {
+            
+            return _digit3.ToString() + _digit4.ToString() + _digit1.ToString() + _digit2.ToString();
+        }
+
+        
+    }
+}
---
+++ EnforcingPrivacyWithCryptography/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnforcingPrivacyWithCryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a 4 digit number to encrypt: ");
+            var number = Console.ReadLine();
+
+            Encryption Enum = new Encryption(number);
+            Console.WriteLine("The number {0} is encrypted to {1}", number, Enum.EncryptedNumber());
+
+
+
+           
+
+            Console.Write("Enter a 4 digit number to decrypt: ");
+            var decryption = Console.ReadLine();
+
+            Decryption dnum = new Decryption(decryption);
+            Console.WriteLine("The number {0} is decrypted to {1}", decryption, dnum.DecryptedNumber());
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
